ec1087245b62eed1bb81071ab55b67a22bd2d947
LiJunpeng <lijunpeng91@gmail.com>
2016-12-01 14:54:59 -0500

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var restRouter = require("./routes/rest");
+var indexRouter = require("./routes/index");
+
+
+app.use('/public', express.static(__dirname + "/public"));
+
+app.use('/', indexRouter);
+
+app.use("/api/v1", restRouter);
+
+app.listen(3001);
